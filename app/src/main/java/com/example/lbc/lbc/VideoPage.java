166c42beb08cb62a8c6b7d73984dab4f7daf88c4
libochao <522427432@qq.com>
2016-04-07 12:26:25 +0800

    '?'

---
+++ app/src/main/java/com/example/lbc/lbc/VideoPage.java
@@ -0,0 +1,7 @@
+package com.example.lbc.lbc;
+
+/**
+ * Created by lbc on 2016/4/4.
+ */
+public class VideoPage {
+}
---
+++ app/src/main/java/com/example/lbc/lbc/url/Urls.java
@@ -0,0 +1,7 @@
+package com.example.lbc.lbc.url;
+
+/**
+ * Created by lbc on 2016/4/6.
+ */
+public class Urls {
+}
